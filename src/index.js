5d46cdb31bcd4651ab15f2171e2e48cfbc11eada
Cory Bass <cory.bass.07@gmail.com>
2020-11-03 11:08:55 -0600

    added handleSubmit and tested functionality, need to add fetch call

--- src/index.js
+++ src/index.js
@@ -15,16 +15,23 @@ class App extends React.Component {
     });
   };
 
+  handleSubmit = (e) => {
+    console.log("handlesubmit", this.state.todo);
+    e.preventDefault();
+  };
+
   render() {
     return (
-      <div>
-        <form>
+      <div className="app">
+        <h1>Todo List</h1>
+        <form className="add-todo" onSubmit={this.handleSubmit}>
           <input
             type="text"
             placeholder="Add Todo"
             value={this.state.todo}
             onChange={this.handleChange}
           />
+          <button type="submit">Add</button>
         </form>
       </div>
     );
